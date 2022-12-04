8eea841febd7bd71d03e1425e86470cdfeeb3e5f
talbii <ido@tal.bi>
2022-12-04 17:29:06 +0200

    Added note

--- b1.cpp
+++ b1.cpp
@@ -3,12 +3,20 @@ using namespace std;
 
 #define int int64_t
 
+/*
+ * Note, this solution finds the 3 biggest-calorie elves in O(log n) time, 
+ * where n is the number of elves. This is ignoring the time cost of reading the input.
+ *
+ * It is possible, whilst reading the input, to find the three-biggest, however
+ * implementing this is much more tedious.
+ * */
+
 signed main(void) {
     string x;
     set<int> s;
     int sum = 0;
 
-    while(getline(cin, x)) {
+    while(getline(cin, x)) { 
         if(x.size() == 0) {
             s.insert(sum);
             sum = 0;
